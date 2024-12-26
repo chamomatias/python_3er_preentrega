c866ae705ebe1bea29ecc0deeec93ce0c9582634
Matias Chamorro <chamomatias@hotmail.com>
2024-12-26 14:50:39 -0300

    Admin configurado

--- proy_python/app/admin.py
+++ proy_python/app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from app.models import unidad_de_negocio, producto, cliente
 
 # Register your models here.
+@admin.register(unidad_de_negocio)
+class unidad_de_negocioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'provincia')
+    search_fields = ('nombre', 'provincia')
+
+admin.site.register(producto)
+class productoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'unidad_de_negocio')
+    search_fields = ('nombre', 'unidad_de_negocio')
+
+admin.site.register(cliente)
+class clienteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'unidad_de_negocio')
+    search_fields = ('nombre', 'unidad_de_negocio')
--- proy_python/app/models.py
+++ proy_python/app/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
-# Create your models here.
 class unidad_de_negocio(models.Model):
     nombre = models.CharField(max_length=40)
     provincia = models.CharField(max_length=40)
+    def __str__(self):
+        return f"{self.id} | {self.nombre} | {self.provincia}"
 
 class producto(models.Model):
     nombre = models.CharField(max_length=40)
     unidad_de_negocio = models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.id} | {self.nombre} | {self.unidad_de_negocio}"
 
 class cliente(models.Model):
     nombre = models.CharField(max_length=40)
     unidad_de_negocio = models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.id} | {self.nombre} | {self.unidad_de_negocio}"
