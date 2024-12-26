24cbf8aaf6c7ad19eda9f0811e2224cd38a47960
Matias Chamorro <chamomatias@hotmail.com>
2024-12-26 00:48:53 -0300

    Administrador configurado)

--- proy_python/app/admin.py
+++ proy_python/app/admin.py
@@ -3,7 +3,17 @@ from .models import unidad_de_negocio, producto, cliente
 
 
 # Register your models here.
-admin.site.register(unidad_de_negocio)
+@admin.register(unidad_de_negocio)
+class unidad_de_negocioAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "provincia")
+    ordering = ("id","nombre", "provincia")
 
-admin.site.register(producto)
-admin.site.register(cliente)
+@admin.register(producto)
+class productoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "unidad_de_negocio")
+    ordering = ("id","nombre", "unidad_de_negocio")
+
+@admin.register(cliente)
+class clienteAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "unidad_de_negocio")
+    ordering = ("id","nombre", "unidad_de_negocio")
