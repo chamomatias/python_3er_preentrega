a4ec0daf35b9d694c5e651d8309fd0250bdead12
Matias Chamorro <chamomatias@hotmail.com>
2024-12-26 01:00:49 -0300

    Eliminar registros de modelos en el administrador y métodos __str__ en los modelos

--- proy_python/app/admin.py
+++ proy_python/app/admin.py
@@ -1,19 +1,3 @@
 from django.contrib import admin
-from .models import unidad_de_negocio, producto, cliente
-
 
 # Register your models here.
-@admin.register(unidad_de_negocio)
-class unidad_de_negocioAdmin(admin.ModelAdmin):
-    list_display = ("id", "nombre", "provincia")
-    ordering = ("id","nombre", "provincia")
-
-@admin.register(producto)
-class productoAdmin(admin.ModelAdmin):
-    list_display = ("id", "nombre", "unidad_de_negocio")
-    ordering = ("id","nombre", "unidad_de_negocio")
-
-@admin.register(cliente)
-class clienteAdmin(admin.ModelAdmin):
-    list_display = ("id", "nombre", "unidad_de_negocio")
-    ordering = ("id","nombre", "unidad_de_negocio")
--- proy_python/app/models.py
+++ proy_python/app/models.py
@@ -7,17 +7,11 @@ from django.db import models
 class unidad_de_negocio(models.Model):
     nombre = models.CharField(max_length=40)
     provincia = models.CharField(max_length=40)
-    def __str__(self):
-        return f"{self.id} | {self.nombre} | {self.provincia}"
 
 class producto(models.Model):
     nombre = models.CharField(max_length=40)
     unidad_de_negocio = models.CharField(max_length=20)
-    def __str__(self):
-        return f"{self.id} | {self.nombre} | {self.unidad_de_negocio}"
 
 class cliente(models.Model):
     nombre = models.CharField(max_length=40)
     unidad_de_negocio = models.CharField(max_length=20)
-    def __str__(self):
-        return f"{self.id} | {self.nombre} | {self.unidad_de_negocio}"
