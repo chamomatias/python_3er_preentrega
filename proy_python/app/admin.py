eb251267b19a4a9dacf811fa690e5c60d5bce3f2
Matias Chamorro <chamomatias@hotmail.com>
2024-12-26 00:22:20 -0300

    Creacion del administrador

--- proy_python/app/admin.py
+++ proy_python/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import unidad_de_negocio, producto, cliente
+
 
 # Register your models here.
+admin.site.register(unidad_de_negocio)
+
+admin.site.register(producto)
+admin.site.register(cliente)
--- proy_python/app/models.py
+++ proy_python/app/models.py
@@ -7,11 +7,17 @@ from django.db import models
 class unidad_de_negocio(models.Model):
     nombre = models.CharField(max_length=40)
     provincia = models.CharField(max_length=40)
+    def __str__(self):
+        return f"{self.id} | {self.nombre} | {self.provincia}"
 
 class producto(models.Model):
     nombre = models.CharField(max_length=40)
     unidad_de_negocio = models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.id} | {self.nombre} | {self.unidad_de_negocio}"
 
 class cliente(models.Model):
     nombre = models.CharField(max_length=40)
     unidad_de_negocio = models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.id} | {self.nombre} | {self.unidad_de_negocio}"
